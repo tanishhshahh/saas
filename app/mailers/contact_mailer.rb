27bbaa8dd0120130701fcb5dc3f4e57a546ffd35
Tanish Shah <tanibshah@gmail.com>
2021-08-25 13:04:51 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::Base
     default to: 'tanish.shah@spit.ac.in'
     
-    def contact_email(name, email, comments)
+    def contact_email(name, email, body)
         @name = name
         @email = email
-        @comments = comments
+        @body = body
         
         mail(from: email, Subject:'Contact Form Message')
     end
